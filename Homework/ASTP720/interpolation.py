f1950b4b10c415270f27ef4b8036377bb258b77c
RyanWills16 <raw3549@rit.edu>
2020-01-28 09:09:01 -0500

    Working update to linear interpolation method

--- Homework/ASTP720/interpolation.py
+++ Homework/ASTP720/interpolation.py
@@ -7,26 +7,32 @@ Created on Mon Jan 27 19:44:31 2020
 """
 
 
-x = [1,2,5,8,10,11]
-y = [1,3,7,9,13,14]
-
-def linpiecewise(x, y):
+def linterp(x, y):
     
-    for ind, i in enumerate(x):
-        if x[ind-1] < x[ind]:
-            
-            def calc_point(x_p):
-                if x[ind-1] < x_p < x[ind]:
-                    return(float(y[ind-1] * (1 - ((x_p - x[ind-1])/(x[ind] - x[ind-1]))) + y[ind] * ((x_p - x[ind-1])/(x[ind] - x[ind-1]))))
-            return calc_point
+    '''
+    '''
+         
+    def calcpoint(x_p): # Function that takes x_p as input, which is the x coordinate of the desired y coordinate
         
-numbers = linpiecewise(x, y)
-x2 = numbers(10.5)         
+        '''
+        '''
+        
+        for ind, i in enumerate(x): # iterating through the list, keeping trak of indicy
+            
+            if x[ind-1] < x_p < x[ind] and y[ind-1] == y[ind]: # accounting for the possibility of a horizontal line between two points
+                
+                return(y[ind])
+                
+                
+            if x[ind-1] < x_p < x[ind]: # if the given point falls between two x coordinates, use those in the calculation
+                
+                # equation for finding the y coordinate of the given x coordinate, x_d is subbed into the equation already.
+                y_p = y[ind-1] * (1 - ((x_p - x[ind-1])/(x[ind] - x[ind-1]))) + y[ind] * ((x_p - x[ind-1])/(x[ind] - x[ind-1]))
+                
+                return(y_p)
+                
+    return calcpoint # returns the callable function
 
-x_p = 10.5       
-for ind, i in enumerate(x):
-    if x[ind-1] < x[ind]:
-        if x[ind-1] < x_p < x[ind]:
-            x_new = y[ind-1] * (1 - ((x_p - x[ind-1])/(x[ind] - x[ind-1]))) + y[ind] * ((x_p - x[ind-1])/(x[ind] - x[ind-1]))
+
         
         
