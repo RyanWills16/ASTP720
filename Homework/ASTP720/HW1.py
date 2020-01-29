0854adcff018ef510702bd42c828bc9dc49a681d
RyanWills16 <raw3549@rit.edu>
2020-01-28 22:39:27 -0500

    updated version of the linear interpolation function plus some testing in HW.py

--- Homework/ASTP720/HW1.py
+++ Homework/ASTP720/HW1.py
@@ -30,23 +30,35 @@ rt.secant(func, -2, -1, verbose = True, numiter = True)
 
 # Problem #2
 
-x = np.arange(-4.5,5, 0.5)
+x = np.arange(-4.5,5, 0.5) # randomly chosen range of numbers
 
-y =  2*x + 11 -x**2
+y =  2*x + 11 -x**2 # randomly chosen quadratic function to get a set of y values
 
-x2 = []
-y2 = []
 
-for i in np.arange(-4.5,x[-1],0.25):
-    newpoint = ip.linterp(x,y)
-    y_new = newpoint(i)
+p = ip.linterp(x,y) # call the linear interpolation function to prime it with sets of points x and y
+
+p(-4.5, verbose = True) # interpolate a random point to check that the function works
+
+
+x2 = [] # defining a new empty set of x values
+y2 = [] # defining a new empty set of y values
+
+for i in np.arange(-4.5,x[-1],0.25):    # iterating over a range of new x points at a higher resolution
+    newpoint = ip.linterp(x,y)  # priming the function with the original x, y data points
+    y_new = newpoint(i)         # using linear interpolation to find new x data points
     
-    x2.append(i)
-    y2.append(y_new)
+    x2.append(i)        # appending new points to empty array
+    y2.append(y_new)    # appending new y points to empty array
     
-plt.scatter(x2,y2, marker = 'o', edgecolors = 'r', facecolors = 'none')
+plt.scatter(x2,y2, marker = 'o', edgecolors = 'r', facecolors = 'none', label = 'interpolated points') # plot new interpolated points
 
-plt.scatter(x,y, marker = 'o', edgecolors = 'b', facecolors = 'none')
+plt.scatter(x,y, marker = 'o', edgecolors = 'b', facecolors = 'none', label = 'Original points') # plot original points in a different color
+
+plt.legend()
+
+# Problem 3
+
+
 
 
 
