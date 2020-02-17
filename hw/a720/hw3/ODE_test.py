2e1981ebd9d7555d8df53b1d8ec15c666c460f56
RyanWills16 <59893519+RyanWills16@users.noreply.github.com>
2020-02-16 23:39:06 -0500

    working rk4 method

--- hw/a720/hw3/ODE_test.py
+++ hw/a720/hw3/ODE_test.py
@@ -10,13 +10,12 @@ import matplotlib.pyplot as plt
 import diffeq as df
 from scipy.integrate import odeint
 
-def examplepend(y,t,b,c):
-    theta,omega = y
+def examplepend(t,y,b=0.25,c=5.0):
+    theta, omega = y 
     dydt = [omega,-b*omega - c*np.sin(theta)]
     return dydt
 
-
-def mypend(t,y,y1,b=0.25,c=5.0):
+def pend(t,y,y1,b=0.25,c=5.0):
     theta = y 
     omega = y1
     dydt = [omega,-b*omega - c*np.sin(theta)]
@@ -33,7 +32,7 @@ t = np.linspace(0,10,101)
 sol1 = odeint(examplepend, y0, t, args = (b,c))
 
 # Testing Euler method
-theta1, omega1, time = df.euler(mypend, y0,t, step = 0.01)    
+theta1, omega1, time = df.euler(pend, y0,t, step = 0.01)    
 
 error_theta1 = sol1[:,0] - theta1 # comparing two methods by finding difference
 error_omega1 = sol1[:,1] - omega1
@@ -46,7 +45,7 @@ plt.plot(t,error_omega1, label = 'Omega Difference')
 plt.legend()
 
 # Testing Heun's Method
-theta2, omega2, time = df.heun(mypend, y0, t)
+theta2, omega2, time2 = df.heun(mypend, y0, t)
 
 error_theta2 = sol1[:,0] - theta2 # comparing two methods by finding difference
 error_omega2 = sol1[:,1] - omega2
@@ -56,4 +55,17 @@ plt.plot(t, theta2, label = 'Theta')
 plt.plot(t, omega2, label = 'Omega')
 plt.plot(t, error_theta2, label = 'eTheta')
 plt.plot(t, error_omega2, label = 'eOmega')
+plt.legend()
+
+# Testing rk4 method
+theta3, omega3, time3 = df.rk4(mypend, y0, t)
+
+error_theta3 = sol1[:,0] - theta3 # comparing two methods by finding difference
+error_omega3 = sol1[:,1] - omega3
+
+plt.figure()
+plt.plot(t, theta3, label = 'Theta')
+plt.plot(t, omega3, label = 'Omega')
+plt.plot(t, sol1[:,0], label = 'eTheta')
+plt.plot(t, sol1[:,1], label = 'eOmega')
 plt.legend()
--- hw/a720/hw3/diffeq.py
+++ hw/a720/hw3/diffeq.py
@@ -66,3 +66,41 @@ def heun(func, initial, time, step = None):
             
             
     return f,y_prime, time
+
+def rk4(func, initial, time, step = None):
+    if not step == None:
+        import numpy as np
+        h = step
+        n = ((time[-1] - time[0])/h + 1)
+        time = np.linspace(time[0], time[-1],n)
+        
+    else:
+        h = (time[-1] - time[0])/(len(time)-1)
+        
+    f = []
+    y_prime = []
+    f.append(initial[0])
+    y_prime.append(initial[1])
+    for ind, t in enumerate(time):
+        if ind > 0:
+            
+            k1_dzdt = func(time[ind -1], f[ind-1], y_prime[ind-1])[1]
+            k1_z = func(time[ind-1], f[ind-1],  y_prime[ind-1])[0]
+            
+            k2_dzdt = func(time[ind -1] + h/2, f[ind-1] + k1_z*h/2, y_prime[ind-1]+k1_dzdt*h/2)[1]
+            k2_z = func(time[ind -1] + h/2,f[ind-1] + k1_z*h/2,y_prime[ind-1]+k1_dzdt*h/2)[0]
+            
+            k3_dzdt = func(time[ind-1] + h/2, f[ind-1] + k2_z*h/2, y_prime[ind-1] + k2_dzdt*h/2)[1]
+            k3_z = func(time[ind-1] + h/2, f[ind-1] + k2_z*h/2, y_prime[ind-1] + k2_dzdt*h/2)[0]
+            
+            k4_dzdt = func(t, f[ind-1] + k3_z*h/2, y_prime[ind-1] + k3_dzdt*h/2)[1]
+            k4_z = func(t, f[ind-1] + k3_z*h/2, y_prime[ind-1] + k3_dzdt*h/2)[0]
+            
+            dzdt = y_prime[ind-1] + (1/6)*h*(k1_dzdt + 2*k2_dzdt + 2*k3_dzdt + k4_dzdt)
+            z = f[ind -1] + (1/6)*h*(k1_z + 2*k2_z + 2*k3_z + k4_z)
+            
+            y_prime.append(dzdt)
+            f.append(z)
+            
+            
+    return f,y_prime, time
